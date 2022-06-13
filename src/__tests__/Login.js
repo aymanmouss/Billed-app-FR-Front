b5c8f540a572f1c28e1f3a4703a4c68de4c0df3f
aymanmouss <ayman@moussa.uk>
2022-06-13 13:33:31 +0200

    Bug report fixed

--- src/__tests__/Login.js
+++ src/__tests__/Login.js
@@ -157,7 +157,8 @@ describe("Given that I am a user on login page", () => {
       expect(screen.getByTestId("form-admin")).toBeTruthy();
     });
   });
-
+  //***************************** [Bug report] - Login ***********************************
+  // In login.js, we have corrected the data-testid to admin-email-input
   describe("When I do fill fields in correct format and I click on admin button Login In", () => {
     test("Then I should be identified as an HR admin in app", () => {
       document.body.innerHTML = LoginUI();
